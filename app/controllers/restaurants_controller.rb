57709024ff3ac009b1e531b1cd62d4d4efad409a
John Carlo Lopez <condottiere.jc@gmail.com>
2018-01-23 13:38:28 -0500

    add CRUD functionality needed in Restaurants_controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,63 @@
 class RestaurantsController < ApplicationController
+  before_action :new_restaurant, only: [:new, :create]
+  before_action :load_restaurant, only: [:show, :edit, :update,  :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "New restaurant successfully added"
+     redirect_to restaurant_path(@restaurant.id)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "Restaurant successfully updated"
+     redirect_to restaurant_path(@restaurant.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    flash[:notice] = "Restaurant successfully deleted"
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def new_restaurant
+    @restaurant = Restaurant.new
+  end
+
+  def load_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :city, :price_range, :summary, :menu)
+    # t.string   "name"
+    # t.string   "address"
+    # t.string   "city"
+    # t.integer  "price_range"
+    # t.text     "summary"
+    # t.text     "menu"
+  end
 end
